4703625d87593b1f7a4229a2ed344d7508874eee
John Byers <jobyers@utilitywarehouse.co.uk>
2022-02-23 09:47:40 +0000

    update dev deps

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .DEFAULT: test
 
+lint:
+	./node_modules/.bin/eslint --fix . && ./node_modules/.bin/prettier --write './*.js'
+
 test:
 	./node_modules/.bin/mocha -r tests/.bootstrap.js tests
 
--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const authorization = (entity, action) => {
-	const verify = (auth, entityId) => {
-		if (!auth || !Array.isArray(auth.scopes)) {
-			return false;
-		}
+  const verify = (auth, entityId) => {
+    if (!auth || !Array.isArray(auth.scopes)) {
+      return false;
+    }
 
-		if (typeof entityId !== 'string' || entityId.length === 0) {
-			return false;
-		}
+    if (typeof entityId !== "string" || entityId.length === 0) {
+      return false;
+    }
 
-		const personal = `${entity}.${entityId}.${action}`;
-		const wildcard = `${entity}.*.${action}`;
+    const personal = `${entity}.${entityId}.${action}`;
+    const wildcard = `${entity}.*.${action}`;
 
-		return auth.scopes.includes(personal) || auth.scopes.includes(wildcard);
-	};
+    return auth.scopes.includes(personal) || auth.scopes.includes(wildcard);
+  };
 
-	const middleware = param => (req, res, next) => {
-		if (!verify(req.auth, req.params[param])) {
-			throw new AuthorizationError();
-		}
+  const middleware = (param) => (req, res, next) => {
+    if (!verify(req.auth, req.params[param])) {
+      throw new AuthorizationError();
+    }
 
-		next();
-	};
+    next();
+  };
 
-	return { verify, middleware }
+  return { verify, middleware };
 };
 
 class AuthorizationError extends Error {
-	constructor() {
-		super();
-		this.status = 401;
-		this.message = 'Unauthorized'
-	}
+  constructor() {
+    super();
+    this.status = 401;
+    this.message = "Unauthorized";
+  }
 }
 
 module.exports = authorization;
